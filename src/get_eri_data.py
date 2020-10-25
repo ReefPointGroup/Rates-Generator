8f358414f29051eb2ce9ac5f4327aa564586ffe9
JLeitzinger <elaboratejohnny@gmail.com>
2020-10-25 15:53:42 -0400

    Working raw data
    Moves raw data from downloads and converts it to csv in 'processed' folder

--- src/get_eri_data.py
+++ src/get_eri_data.py
@@ -11,12 +11,18 @@ For initial running, make sure to create auth/upass.txt with username, password 
 
 from selenium import webdriver
 import pandas as pd
-import time
+import time, shutil, os
 
 url = 'https://online.erieri.com/Account/Login'
 ar_url = 'https://online.erieri.com/SA/AdvancedReports/'
 
+#Lazy find_element_by_xpath
+def f_xpath(driver, xpath):
+    loc = driver.find_element_by_xpath(xpath)
+    return(loc)
 
+
+#Reads the upass file created, starts driver, logs in 
 def eri_login(url):
     f=pd.read_csv('auth/upass.txt', sep=',', header=None, index_col=None)
     usr = f.iloc[0][0].strip()
@@ -33,10 +39,8 @@ def eri_login(url):
     return(driver)
 
 
-def f_xpath(driver, xpath):
-    loc = driver.find_element_by_xpath(xpath)
-    return(loc)
 
+#downoads advanced report for saved job titles
 def get_advanced_report(url, ar_url):
 
     driver = eri_login(url)
@@ -45,6 +49,51 @@ def get_advanced_report(url, ar_url):
     f_xpath(driver, '//*[@id="Toolbar #btnExcelExport"]/i').click()
     time.sleep(3)
     f_xpath(driver, '//*[@id="btnOK_AdvancedReportsToExcelModal1"]').click()
+    time.sleep(2)
+    driver.close()
+
+
+def move_from_downloads(fname):
+    dl_fold = os.path.join(os.path.expanduser('~'), 'downloads')
+    fold_list = [x for x in os.listdir(dl_fold) if fname in x]
+    max_mtime = 0 
+    for file in fold_list:
+        temp_path = os.path.join(dl_fold, file)
+        mtime = os.stat(temp_path).st_mtime
+        if mtime > max_mtime:
+            max_mtime = mtime
+            max_file = file
+    
+    source = os.path.join(dl_fold, max_file)
+    dest = 'data/eri/raw/'
+    
+    if not os.path.exists(dest):
+        os.makedirs(dest)
+      
+
+    dest_file = os.path.join(dest, fname+'.xlsx')
+    
+    if os.path.exists(dest_file):
+        os.remove(dest_file)
+    
+    shutil.move(source, dest)
+    os.rename(os.path.join(dest, max_file), dest_file)
+
+
+def wrangle_eri():
+    
+    df = pd.read_excel('data/eri/raw/AdvancedJobReport.xlsx', 'Advanced Job Report', skiprows=7, usecols = 'A:L')
+    
+    
+    
+    eri_loc = 'data/eri/processed/'
+    #saving space for wrangling
+    if not os.path.exists(eri_loc):
+        os.makedirs(eri_loc)
+        
+    df.to_csv('data/eri/processed/ERI_Rates.csv')
 
 
 get_advanced_report(url, ar_url)
+move_from_downloads('AdvancedJobReport')
+wrangle_eri()
